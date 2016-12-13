59f2fac24b1f8a9fce09e99848ec08ef0055edea
Grant Wang <yidegrant@gmail.com>
2016-12-13 12:08:14 -0500

    Added some scenes to build manager. Rewrote next scene script and datalogger

---
+++ TheSpoopiestTinder/Assets/Scripts/Datalogger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Datalogger : MonoBehaviour {
+    public ArrayList matches = new ArrayList();
+	// Use this for initialization
+	void Start () {
+	
+	}
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
---
+++ TheSpoopiestTinder/Assets/Scripts/NextScene.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void loadNextScene()
+    {
+        if (transform.parent.gameObject.name == "Eh")
+        {
+            GameObject.Find("Datalogger").GetComponent<Datalogger>().matches.Add(true);
+            Debug.Log("You said yes!");
+        }
+        else
+        {
+            GameObject.Find("Datalogger").GetComponent<Datalogger>().matches.Add(false);
+            Debug.Log("You said no!");
+        }
+        int current = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(current++);
+    }
+}

5fb70ed2bc2bb0125d0b2246aa4d2cedf592c31f
Tyler Finley <tcf234@nyu.edu>
2016-12-13 10:22:43 -0500

    third profile things

---
+++ TheSpoopiestTinder/Assets/Scripts/ClickRight.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ClickRight : MonoBehaviour {
+    public Button rightPic;
+    public GameObject image1;
+    public GameObject image2;
+    public GameObject image3;
+
+	// Use this for initialization
+	void Start () {
+        Button btn = rightPic.GetComponent<Button>();
+        btn.onClick.AddListener(moveRight);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void moveRight()
+    {
+
+    }
+}

470dbe826d19c471bd4b184ffd26114b6ba0fc5d
Tyler Finley <tcf234@nyu.edu>
2016-12-14 10:45:37 -0500

    audio scripts

--- TheSpoopiestTinder/Assets/Scripts/NextScene.cs
+++ TheSpoopiestTinder/Assets/Scripts/NextScene.cs
@@ -4,10 +4,18 @@ using UnityEngine.SceneManagement;
 
 public class NextScene : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    AudioClip ehSound;
+    AudioClip noSound;
+    GameObject noisePlayer;
+    //public GameObject ehButton;
+    //public GameObject noButton;
+
+    // Use this for initialization
+    void Start () {
+        ehSound = Resources.Load<AudioClip>("ehSound");
+        noSound = Resources.Load<AudioClip>("noSound");
+        noisePlayer = GameObject.Find("ResponseSource");
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -17,14 +25,33 @@ public class NextScene : MonoBehaviour {
     {
         if (transform.CompareTag("Yes"))
         {
-            GameObject.Find("Datalogger").GetComponent<Datalogger>().matches.Add(true);
+            
             Debug.Log("You said yes!");
+            noisePlayer.GetComponent<AudioSource>().PlayOneShot(ehSound);
+            //ehButton.SetActive(false);
+            //noButton.SetActive(false);
+            StartCoroutine(sceneWait());
+
         }
         else
         {
+            GameObject.Find("Datalogger").GetComponent<Datalogger>().matches.Add(false);
             Debug.Log("You said no!");
+            noisePlayer.GetComponent<AudioSource>().PlayOneShot(noSound);
+            //ehButton.SetActive(false);
+            //noButton.SetActive(false);
+            StartCoroutine(sceneWait());
+            
         }
+        StartCoroutine(sceneWait());
+        //int current = SceneManager.GetActiveScene().buildIndex;
+        //SceneManager.LoadScene(current+1);
+    }
+
+    IEnumerator sceneWait()
+    {
+        yield return new WaitForSecondsRealtime(2.75f);
         int current = SceneManager.GetActiveScene().buildIndex;
-        SceneManager.LoadScene(current+1);
+        SceneManager.LoadScene(current + 1);
     }
 }
---
+++ TheSpoopiestTinder/Assets/Scripts/backgroundMusic.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class backgroundMusic : MonoBehaviour {
+
+    public GameObject soundPlayer;
+    public GameObject soundPlayer2;
+
+	// Use this for initialization
+	void Awake () {
+        soundPlayer = GameObject.Find("RoomSounds");
+        soundPlayer2 = GameObject.Find("RoomSounds (1)");
+        DontDestroyOnLoad(soundPlayer);
+        DontDestroyOnLoad(soundPlayer2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
